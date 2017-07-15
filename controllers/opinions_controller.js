d73cdf756e147d808c762249d3c9152c71101f33
Jared Moscrip <jaredmoscrip@gmail.com>
2017-07-15 10:49:00 -0500

    comments

--- controllers/opinions_controller.js
+++ controllers/opinions_controller.js
@@ -11,22 +11,27 @@ router.get("/", function(req, res) {
     res.render("index");
 });
 
+//will also render index page D:
 router.get("/about", function(req, res) {
     res.render("index");
 });
 
+// render projecs
 router.get("/projects", function(req, res) {
     res.render("projects");
 });
 
+// render a project, the only real product...
 router.get("/scraped", function(req, res) {
     res.render("project2");
 });
 
+// render the error projects cuz i haven't added them yet. 
 router.get("/error", function(req, res) {
     res.render("error");
 });
 
+// render contacts if there is any. 
 router.get("/contact", function(req, res) {
     db.Contacts.findAll({}).then(function(contact) {
         console.log(contact);
@@ -36,7 +41,7 @@ router.get("/contact", function(req, res) {
     });
 });
 
-//Router to update the current doctor and diagnosis for the logged in patient
+//post call to save a new contact to our database. 
 router.post("/contact", function(req, res) {
 
     console.log(req.body);
@@ -49,32 +54,4 @@ router.post("/contact", function(req, res) {
     // });
 });
 
-//Router to update to add an admin based on the current Patients table
-// router.put("/", function(req, res) {
-//     // console.log(req.body);
-//     // console.log(req.body.id);
-//     db.Patients.update({
-//         isAdmin: req.body.isAdmin
-//         }, {
-//         where: {
-//             patient_id: req.params.id
-//         }
-//     }).then(function() {
-//         res.redirect("/admin");
-//     });
-// });
-
-//Router to render all of the available doctors on the docs page
-// router.get("/docs", function(req, res) {
-//     db.Doctors.findAll({}).then(function(results) {
-//         res.render("docs", { doctors: results });
-//     });
-// });
-
-// router.get("/about", function(req, res) {
-//     res.render("about");
-// });
-
-
- 
 module.exports = router;
